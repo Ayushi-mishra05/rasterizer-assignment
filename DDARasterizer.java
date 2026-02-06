2a5552066e0879ce77dfee26f6fa89cfd0906021
Ayushi Mishra <ayushiiii102@gmail.com>
2026-02-06 19:25:26 +0530

    Add DDA Rasterizer

---
+++ DDARasterizer.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class DDARasterizer implements LineRasterizer {
+
+    @Override
+    public Point[] rasterize(Point p1, Point p2) {
+
+        int x1 = p1.x;
+        int y1 = p1.y;
+        int x2 = p2.x;
+        int y2 = p2.y;
+
+        ArrayList<Point> points = new ArrayList<>();
+
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+
+        int steps = Math.max(Math.abs(dx), Math.abs(dy));
+
+        float xInc = dx / (float) steps;
+        float yInc = dy / (float) steps;
+
+        float x = x1;
+        float y = y1;
+
+        for (int i = 0; i <= steps; i++) {
+            points.add(new Point(Math.round(x), Math.round(y)));
+            x += xInc;
+            y += yInc;
+        }
+
+        return points.toArray(new Point[0]);
+    }
+}
