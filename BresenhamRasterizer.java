443262cc87b016e85ff395b1fe3c4ba75643d6f2
Ayushi Mishra <ayushiiii102@gmail.com>
2026-02-06 19:09:21 +0530

    Extracted start and end coordinates

--- BresenhamRasterizer.java
+++ BresenhamRasterizer.java
@@ -2,7 +2,14 @@ import java.util.ArrayList;
 
 class BresenhamRasterizer implements LineRasterizer {
     public Point[] rasterize(Point p1, Point p2) {
-        return new Point[]{ new Point(0,0), new Point(2,3) };
+        int x1 = p1.x;
+    int y1 = p1.y;
+    int x2 = p2.x;
+    int y2 = p2.y;
+
+    
+    return new Point[]{ new Point(x1, y1), new Point(x2, y2) }; 
+       
     }
 
 }
