61b67f166417be2ef6036a58e07493bd57e4f62d
Ayushi Mishra <ayushiiii102@gmail.com>
2026-02-06 19:10:05 +0530

    Added ArrayList to store points
    Refactor rasterize method to use ArrayList for points.

--- BresenhamRasterizer.java
+++ BresenhamRasterizer.java
@@ -2,13 +2,17 @@ import java.util.ArrayList;
 
 class BresenhamRasterizer implements LineRasterizer {
     public Point[] rasterize(Point p1, Point p2) {
-        int x1 = p1.x;
+    int x1 = p1.x;
     int y1 = p1.y;
     int x2 = p2.x;
     int y2 = p2.y;
 
-    
-    return new Point[]{ new Point(x1, y1), new Point(x2, y2) }; 
+    ArrayList<Point> points = new ArrayList<>();
+
+    points.add(new Point(x1, y1));
+    points.add(new Point(x2, y2));
+
+    return points.toArray(new Point[0]);
        
     }
 
