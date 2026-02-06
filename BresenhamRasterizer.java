f4a97b06d27bde8e26c110a52a90ed6538db4c75
Ayushi Mishra <ayushiiii102@gmail.com>
2026-02-06 19:12:58 +0530

    Completed Bresenham line algorithm

--- BresenhamRasterizer.java
+++ BresenhamRasterizer.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 class BresenhamRasterizer implements LineRasterizer {
     public Point[] rasterize(Point p1, Point p2) {
+
     int x1 = p1.x;
     int y1 = p1.y;
     int x2 = p2.x;
@@ -15,12 +16,31 @@ class BresenhamRasterizer implements LineRasterizer {
     int sx = (x1 < x2) ? 1 : -1;
     int sy = (y1 < y2) ? 1 : -1;
 
-   
-    points.add(new Point(x1, y1));
-    points.add(new Point(x2, y2));
+    int err = dx - dy;
+
+    while (true) {
+        points.add(new Point(x1, y1));
+
+        if (x1 == x2 && y1 == y2)
+            break;
+
+        int e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
 
     return points.toArray(new Point[0]);
+}
+
        
     }
 
-}
+
